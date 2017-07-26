141c25c84f5fd4f9e02c90539520f2e429a6abd6
chenxiaoxin <1205942067@qq.com>
2017-07-26 16:13:18 +0800

    初次提交git

---
+++ git/src/git/Test.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package git;
+
+/** @Description: 
+ *  @author ³ÂÏşöÎ
+ *  @date 2017Äê7ÔÂ26ÈÕ
+ *
+ */
+public class Test {
+public static void main(String[] args) {
+	
+	System.out.println("hello Git");
+}
+}
